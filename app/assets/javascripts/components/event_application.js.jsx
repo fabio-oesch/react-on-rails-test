4128520916398040a45027db7a5d2923d4b91c70
O'Learry? <fabio.trash.mail@gmail.com>
2018-01-11 13:40:43 +0000

    stuff added

--- app/assets/javascripts/components/event_application.js.jsx
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -1,4 +1,22 @@
 class EventApplication extends React.Component {
+    getInitialState() {
+        return { events: [] };
+    }
+    componentDidMount() {
+        this.getDataFromApi();
+    }
+    getDataFromApi() {
+        var self = this;
+        $.ajax({
+            url: '/api/events',
+            success: function(data) {
+                self.setState({ events: data });
+            },
+            error: function (xhr, status, error) {
+                alert('Cannot get data from API: ', error);
+            }
+        })
+    }
     render () {
         return (
             <div className="container">
@@ -8,7 +26,7 @@ class EventApplication extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-md-12">
-                        <EventTable />
+                        <EventTable events={this.state.events} />
                     </div>
                 </div>
             </div>
